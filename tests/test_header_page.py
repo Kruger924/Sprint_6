cad12c82e956179936c2fac0c0f9b20efb207dde
Kruger924 <kruger924@yandex.ru>
2026-04-05 15:10:51 +0300

    added import sys

--- tests/test_header_page.py
+++ tests/test_header_page.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append(".")
 import time
 import allure
 import pytest
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append(".")
 import allure
 from data import Users
 from pages.home_page import HomePage, HomePageHeader
