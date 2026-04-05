8cce903c7c48c1948c43f99d21c4a28e7729bd2f
Kruger924 <kruger924@yandex.ru>
2026-04-05 12:44:19 +0300

    added file conftest

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import allure
+import pytest
+from selenium import webdriver
+from data import Urls
+
+@allure.step('Открытие браузера / переход на страницу / закрытие браузера')
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    driver.get(Urls.QA_SCOOTER_URL)
+    yield driver
+    driver.quit()
+
+def pytest_make_parametrize_id(val):
+    return repr(val)
